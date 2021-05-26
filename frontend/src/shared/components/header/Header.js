3674d7b837ba9fdb6a44cdf2686d4b68ee06a292
Md Monirul Islam <mmislam027@gmail.com>
2021-05-26 15:05:34 +0600

    style: Header menu

--- frontend/src/shared/components/header/Header.js
+++ frontend/src/shared/components/header/Header.js
@@ -1,5 +1,5 @@
+import { Avatar } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
-import Badge from '@material-ui/core/Badge';
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
 import Menu from '@material-ui/core/Menu';
@@ -8,12 +8,14 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
+import HomeIcon from '@material-ui/icons/Home';
 import MenuIcon from '@material-ui/icons/Menu';
 import MoreIcon from '@material-ui/icons/MoreVert';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import LoggedInUserMenu from './LoggedInUserMenu';
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -67,99 +69,43 @@ const useStyles = makeStyles((theme) => ({
   },
   sectionDesktop: {
     display: 'none',
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('sm')]: {
       display: 'flex',
     },
   },
   sectionMobile: {
     display: 'flex',
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('sm')]: {
       display: 'none',
     },
+  },
+  menuItemLink: {
+    all: 'inherit',
+    background: 'none',
+    width: '100%',
+    color: 'inherit',
   },
 }));
 
 export default function Header() {
+  const authState = useSelector((state) => state.auth);
+
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
 
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
+  const isAuthenticated = authState.isAuthenticated;
 
   const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
   const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label='show 4 new mails' color='inherit'>
-          <Badge badgeContent={4} color='secondary'>
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label='show 11 new notifications' color='inherit'>
-          <Badge badgeContent={11} color='secondary'>
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label='account of current user'
-          aria-controls='primary-search-account-menu'
-          aria-haspopup='true'
-          color='inherit'
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
 
   return (
     <div className={classes.grow}>
@@ -191,26 +137,20 @@ export default function Header() {
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-            <IconButton aria-label='show 4 new mails' color='inherit'>
-              <Badge badgeContent={4} color='secondary'>
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton aria-label='show 17 new notifications' color='inherit'>
-              <Badge badgeContent={17} color='secondary'>
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              edge='end'
-              aria-label='account of current user'
-              aria-controls={menuId}
-              aria-haspopup='true'
-              onClick={handleProfileMenuOpen}
-              color='inherit'
-            >
-              <AccountCircle />
-            </IconButton>
+            <Link to='/' className={classes.menuItemLink}>
+              <IconButton edge='end' color='inherit'>
+                <HomeIcon />
+              </IconButton>
+            </Link>
+            {!isAuthenticated ? (
+              <Link to='/login' className={classes.menuItemLink}>
+                <IconButton edge='end' color='inherit'>
+                  <AccountCircle />
+                </IconButton>
+              </Link>
+            ) : (
+              <LoggedInUserMenu />
+            )}
           </div>
           <div className={classes.sectionMobile}>
             <IconButton
@@ -225,8 +165,55 @@ export default function Header() {
           </div>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={mobileMenuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileMenuClose}
+      >
+        {isAuthenticated ? (
+          <>
+            <MenuItem style={{ minWidth: '140px' }}>
+              <Link to='/login' className={classes.menuItemLink + ' p-0'}>
+                <IconButton
+                  aria-label='account of current user'
+                  aria-controls='primary-search-account-menu'
+                  aria-haspopup='true'
+                  color='inherit'
+                >
+                  <Avatar
+                    alt='Remy Sharp'
+                    src='/static/images/avatar/1.jpg'
+                    style={{ width: '30px', height: '30px' }}
+                  />
+                </IconButton>
+
+                <p>User</p>
+              </Link>
+            </MenuItem>
+          </>
+        ) : (
+          <>
+            <MenuItem>
+              <Link to='/login' className={classes.menuItemLink + ' p-0'}>
+                <IconButton
+                  aria-label='account of current user'
+                  aria-controls='primary-search-account-menu'
+                  aria-haspopup='true'
+                  color='inherit'
+                >
+                  <AccountCircle />
+                </IconButton>
+
+                <p>Login</p>
+              </Link>
+            </MenuItem>
+          </>
+        )}
+      </Menu>
     </div>
   );
 }
