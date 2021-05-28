0c38d8864d9fb6411336457fd8d9167696b91b9d
Md Monirul Islam <mmislam027@gmail.com>
2021-05-28 09:54:59 +0600

    feat: set auth token

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,21 @@
+import jwtDecode from 'jwt-decode';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
+import { SET_USER } from './store/actions/types';
 import store from './store/store';
+import setAuthToken from './utils/setAuthToken';
+
+const token = localStorage.getItem('auth_token');
+if (token) {
+  const user = jwtDecode(token);
+  setAuthToken(token);
+  store.dispatch({
+    type: SET_USER,
+    payload: { user },
+  });
+}
 
 ReactDOM.render(
   <Provider store={store}>
