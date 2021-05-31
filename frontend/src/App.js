52149b03ec3bfee75cccbaa8a688fb9bdbaaf1fe
Md Monirul Islam <mmislam027@gmail.com>
2021-05-31 16:43:08 +0600

    restart the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
 import { Container } from '@material-ui/core';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Admin from './admin/Admin';
 import './App.css';
-import Home from './public/pages/Home';
-import SinglePost from './public/pages/SinglePost';
-import Header from './shared/components/header/Header';
 
 const App = () => {
   return (
     <Router>
-      <Header />
       <Container>
         <div style={{ padding: '10px 0' }}>
           <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/post/:slug' component={SinglePost} />
-            <Admin />
+            <Route path='/' exact>
+              <h2>Hello world 1</h2>
+            </Route>
           </Switch>
         </div>
       </Container>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,10 @@
-import jwtDecode from 'jwt-decode';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import { SET_USER } from './store/actions/types';
-import store from './store/store';
-import setAuthToken from './utils/setAuthToken';
-
-const token = localStorage.getItem('auth_token');
-setAuthToken(token);
-if (token) {
-  const user = jwtDecode(token);
-
-  if (Date.now() > user.exp) {
-    localStorage.removeItem('auth_token');
-    store.dispatch({
-      type: SET_USER,
-      payload: { user: {} },
-    });
-  } else {
-    store.dispatch({
-      type: SET_USER,
-      payload: { user },
-    });
-  }
-}
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
