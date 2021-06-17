ac166d74411a0d06e3a268f6cefabe7fd7037a16
Md Monirul Islam <mmislam027@gmail.com>
2021-06-17 11:03:29 +0600

    bug: fix home sidebar category

--- frontend/src/public/components/posts/HomePageSingleCategoryPosts.js
+++ frontend/src/public/components/posts/HomePageSingleCategoryPosts.js
@@ -9,6 +9,8 @@ const useStyles = makeStyles({
   postLink: {
     textDecoration: 'none',
     color: '#000',
+    display: 'block',
+    marginBottom: '5px',
   },
   postsWrapper: {
     background: '#F5F5F5',
